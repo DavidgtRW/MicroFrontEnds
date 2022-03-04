1b3bdcbcd28a65457599923c05f4c989114a4316
Andres David Gomez Toledo <agometol@emeal.nttdata.com>
2022-03-03 21:51:43 -0500

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ma',
+})
 
 export default () =>{
     return <div>
