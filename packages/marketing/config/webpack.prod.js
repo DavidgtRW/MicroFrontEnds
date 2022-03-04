282a8c2d60922d19143ed64b1fcd6a6ea76f35cc
Andres David Gomez Toledo <agometol@emeal.nttdata.com>
2022-03-03 20:07:23 -0500

    Added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/',
     },
     plugins: [
         new ModuleFederationPlugin({
